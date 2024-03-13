672627b225abc3911ef5bfb0480676a3f7db70bf
hsobane <hassan.sobane@gmail.com>
2024-03-13 12:50:42 +0000

    ok

--- cub3d.h
+++ cub3d.h
@@ -6,6 +6,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+typedef enum s_texture
+{
+    NO,
+    SO,
+    WE,
+    EA,
+    F,
+    C,
+    EMPTY,
+    MAP
+}   t_texture;
+
+typedef struct s_pareser
+{
+    t_texture       texture;
+    char            *line;
+    struct s_parser *next;
+}   t_parser;
+
 typedef struct s_parser
 {
     char **map;
